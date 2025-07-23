2817d6cf2f82d8de4e63239cd77f8fc7fb50435b
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2025-07-23 16:04:07 +0300

    Initial Commit

---
+++ HammerSystemsFoodDelivery/HammerSystemsFoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HammerSystemsFoodDeliveryApp.swift
+//  HammerSystemsFoodDelivery
+//
+//  Created by Evgeniy Fakhretdinov on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HammerSystemsFoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
